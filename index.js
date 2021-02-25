71d97f95d1fd49c0e4083972c40c9249b9aff25c
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2021-02-25 12:13:30 -0500

    Add dialog flow fulfu=illment

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { WebhookClient } = require('dialogflow-fulfillment')
+const { response } = require('express')
+
 const app = express()
 app.use(bodyParser.json())
 const port = process.env.POST || 3000
 
 
 app.post('/dialogflow-fulfillment', (request, response) => {
-    console.log("test")
+    dialogflowFulfillment(request, response)
 })
 
 app.listen(port, () => {
     console.log("Listening on post" + port)
 })
+
+const dialogflowFulfillment = (request, response) => {
+    const agent = new WebhookClient({ request, response })
+
+    function sayHello(agent) {
+        agent.add("Hi there, this response is coming from heroku")
+    }
+
+    let intentMap = new Map();
+    intentMap.set("Default Welcome Intent", sayHello)
+    agent.handleRequest(intentMap)
+}
